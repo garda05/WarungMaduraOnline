97d408f309c30c210579860243661e589a3fe456
nikenkaziasitumeangg <nikenkaziasitumeangg@gmail.com>
2025-12-18 02:40:05 +0700

    CRUD Barang (create, read, update, delete)

--- Downloads/WarungMaduraOnline-main/WarungMaduraOnline-main/routes/web.php
+++ Downloads/WarungMaduraOnline-main/WarungMaduraOnline-main/routes/web.php
@@ -1,17 +1,25 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Dashboard (wajib login & verified)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+// Semua fitur setelah login
+Route::middleware(['auth'])->group(function () {
+
+    // CRUD BARANG
+    Route::resource('barang', BarangController::class);
+
+    // PROFILE
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
