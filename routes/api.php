0a14fc6801142e1018c51059a8e78bef11e73f94
citrafadillahagung <citrafadilahagung@gmail.com>
2026-01-05 03:39:37 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\BarangController; // TAMBAHAN
+use App\Http\Controllers\Api\BarangController;
 
+// AUTH API
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// PROTECTED API
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    // =======================
     // CRUD API BARANG
-    // =======================
-    Route::get('/barang', [BarangController::class, 'index']);      // GET all
-    Route::post('/barang', [BarangController::class, 'store']);     // CREATE
-    Route::get('/barang/{id}', [BarangController::class, 'show']);  // DETAIL
-    Route::put('/barang/{id}', [BarangController::class, 'update']); // UPDATE
+    Route::get('/barang', [BarangController::class, 'index']);          // GET ALL
+    Route::post('/barang', [BarangController::class, 'store']);         // CREATE
+    Route::get('/barang/{id}', [BarangController::class, 'show']);      // DETAIL
+    Route::put('/barang/{id}', [BarangController::class, 'update']);    // UPDATE
     Route::delete('/barang/{id}', [BarangController::class, 'destroy']); // DELETE
 });
